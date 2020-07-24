fd577b1507684bb89f470656e25d07848cfb3806
mayoko <mayokoex@gmail.com>
2020-07-24 18:00:28 +0900

    ENG-54: tried to modify GitHub actions to check typescript code, but will try it later

--- GUI/src/index.tsx
+++ GUI/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import InputForm from './components/InputForm';
-import Store from './Store';
+import Store from './store';
 
 const container = document.getElementById('contents');
 
